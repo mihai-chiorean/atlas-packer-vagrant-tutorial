4a5649e32525c7444c2f964fae6aa078da5437b5
Mihai Chiorean <mihai@knotch.it>
2016-01-19 12:09:10 -0800

    fix: missing -y in dep installs

--- scripts/dep.sh
+++ scripts/dep.sh
@@ -9,12 +9,12 @@ apt-get -y install curl git
 # You can install anything you need here
 
 # golang
-apt-get install golang
+apt-get -y install golang
 
 # vim
 apt-get remove vim vim-runtime gvim
 apt-get remove vim-tiny vim-common vim-gui-common.
-apt-get install liblua5.1-dev luajit libluajit-5.1 python-dev \
+apt-get -y install liblua5.1-dev luajit libluajit-5.1 python-dev \
             ruby-dev libperl-dev libncurses5-dev libgnome2-dev \
             libgnomeui-dev libgtk2.0-dev libatk1.0-dev libbonoboui2-dev \
             libcairo2-dev libx11-dev libxpm-dev libxt-dev
