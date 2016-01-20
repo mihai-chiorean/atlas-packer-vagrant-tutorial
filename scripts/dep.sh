cfa29dd19f2e6c21f53aba0654cfc0464b37e090
Mihai Chiorean <mihai@knotch.it>
2016-01-20 08:44:24 -0800

    fix: attempt to fix go development environment

--- scripts/dep.sh
+++ scripts/dep.sh
@@ -9,7 +9,7 @@ apt-get -y install curl git
 # You can install anything you need here
 
 # golang
-apt-get -y install golang golang-go
+apt-get -y install golang golang-go gocode godef goimports golint gometalinter gotags gorename asmfmt
 
 # vim
 apt-get -y install vim-syntax-go vim-addon-manager
--- scripts/go.sh
+++ scripts/go.sh
@@ -8,15 +8,15 @@ git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 mkdir /home/vagrant/.vim
 mkdir /home/vagrant/.vim/syntax
 mkdir /home/vagrant/.vim/ftdetect
-mkdir ~/.vim/bundle
+mkdir /home/vagrant/.vim/bundle
 
 chown -R vagrant:vagrant /home/vagrant/.vim
 
 touch ~/.vimrc
 chown vagrant:vagrant ~/.vimrc
 
-cd ~/.vim/bundle
-git clone git://github.com/geekq/vim-go.git
+#cd ~/.vim/bundle
+#git clone git://github.com/geekq/vim-go.git
 
 echo 'set nocompatible              " be iMproved, required  ' >> ~/.vimrc
 echo 'filetype off                  " required               ' >> ~/.vimrc
@@ -31,3 +31,7 @@ mkdir ~/workspace
 chown -R vagrant:vagrant ~/workspace
 
 echo 'export GOPATH=~/workspace' >> ~/.bash_profile
+echo 'export GOBIN=~/workspace/bin' >> ~/.bash_profile
+
+# install development binaries
+vim -cmd '' -c 'GoInstallBinaries' -c 'qa!'
