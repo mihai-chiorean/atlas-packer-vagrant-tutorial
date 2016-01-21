88c61539916111367b270f77f1c0b23883b5ba05
Mihai Chiorean <mihai@knotch.it>
2016-01-21 12:29:24 -0800

    0.0.21

--- scripts/dep.sh
+++ scripts/dep.sh
@@ -15,4 +15,17 @@ apt-get install -y golang golang-go gocode gpm ctags \
     vim-doc vim-scripts golang-go.tools
 
 # vim
-apt-get -y install vim-syntax-go vim-addon-manager
+apt-get -y install software-properties-common
+add-apt-repository -y ppa:thopiekar/pacman
+apt-get update
+apt-get -y install vim-syntax-go vim-addon-manager dirmngr pacman pacman-pm rng-tools
+
+printf '[core] \n
+SigLevel = PackageRequired \n
+Include = /etc/pacman.d/mirrorlist \n
+\n
+[extra] \n
+SigLevel = PackageRequired \n
+Include = /etc/pacman.d/mirrorlist \n' >> /etc/pacman.conf
+
+pacman-key --init
