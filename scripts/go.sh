6a09e5fbfb55680c172bb00a8d5c9995cd7bc098
Mihai Chiorean <mihai@knotch.it>
2016-01-20 21:08:53 -0800

    0.0.20 - fix dotfiles github url

--- scripts/go.sh
+++ scripts/go.sh
@@ -4,7 +4,7 @@
 # setup vim environment
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 git clone https://github.com/fatih/vim-go.git ~/.vim/bundle/vim-go
-git clone git@github.com:mihai-chiorean/dotfiles.git ~/dotfiles
+git clone https://github.com/mihai-chiorean/dotfiles.git ~/dotfiles
 
 cd ~/dotfiles
 make
